ca2a41bd07b4c75fb1c8a0ea0104d95903e8d12d
Tyler Samuelson <tylercsamuelson@gmail.com>
2021-03-12 13:08:44 -0700

    Created a Context Object and connected to Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
+import LanguageContext from "../contexts/LanguageContext.js";
 
 class Button extends Component {
+  // Below syntax is the same as saying Button.contextType = LanguageContext | it attaches to the class!
+  static contextType = LanguageContext;
+
   render() {
     return <button className="ui button primary">Submit</button>;
   }
