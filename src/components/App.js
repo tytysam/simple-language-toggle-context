ea9e566937ea328ab9d9790a1cc80d349273970b
Tyler Samuelson <tylercsamuelson@gmail.com>
2021-03-12 12:49:48 -0700

    Finished basic markup for language toggle. Added UserCreate, Field, and Button components

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import UserCreate from "./UserCreate.js";
 
 class App extends Component {
   state = { language: "english" };
@@ -21,7 +22,7 @@ class App extends Component {
             onClick={() => this.onLanguageChange("dutch")}
           />
         </div>
-        {this.state.language}
+        <UserCreate />
       </div>
     );
   }
