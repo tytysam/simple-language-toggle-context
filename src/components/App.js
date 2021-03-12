c9fab57e0a0af51fc5b178283b124254bf099d80
Tyler Samuelson <tylercsamuelson@gmail.com>
2021-03-12 12:44:09 -0700

    Setup state in App.js for language, and setup initial onClicks

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,27 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  state = { language: "english" };
+
+  onLanguageChange = (language) => {
+    this.setState({ language: language });
+  };
+
   render() {
     return (
       <div className="ui container">
-        <div>Test</div>
+        <div>
+          Select a language:
+          <i
+            className="flag us"
+            onClick={() => this.onLanguageChange("english")}
+          />
+          <i
+            className="flag nl"
+            onClick={() => this.onLanguageChange("dutch")}
+          />
+        </div>
+        {this.state.language}
       </div>
     );
   }
