19836c820e1959beda36c24ba237f125c9a8d541
Tyler Samuelson <tylercsamuelson@gmail.com>
2021-03-13 12:02:48 -0700

    Implemented a Consumer in Button.js component

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,11 +3,18 @@ import LanguageContext from "../contexts/LanguageContext.js";
 
 class Button extends Component {
   // Below syntax is the same as saying Button.contextType = LanguageContext | it attaches to the class!
-  static contextType = LanguageContext;
+  // static contextType = LanguageContext;
 
   render() {
-    const text = this.context === "english" ? "Submit" : "Voorleggen";
-    return <button className="ui button primary">{text}</button>;
+    // const text = this.context === "english" ? "Submit" : "Voorleggen";
+    return (
+      <button className="ui button primary">
+        <LanguageContext.Consumer>
+          {/* Consumer syntax here is in place of this.context syntax used previously, now commented-out */}
+          {(value) => (value === "english" ? "Submit" : "Voorleggen")}
+        </LanguageContext.Consumer>
+      </button>
+    );
   }
 }
 
