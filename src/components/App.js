b825f0c1f370e720df81dbecfd5bf528d03582b9
Tyler Samuelson <tylercsamuelson@gmail.com>
2021-03-13 11:52:42 -0700

    Added Provider for supplying  Context.

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import UserCreate from "./UserCreate.js";
+import LanguageContext from "../contexts/LanguageContext.js";
 
 class App extends Component {
   state = { language: "english" };
@@ -22,7 +23,9 @@ class App extends Component {
             onClick={() => this.onLanguageChange("dutch")}
           />
         </div>
-        <UserCreate />
+        <LanguageContext.Provider value={this.state.language}>
+          <UserCreate />
+        </LanguageContext.Provider>
       </div>
     );
   }
